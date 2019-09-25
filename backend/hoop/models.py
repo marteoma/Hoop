f4cbc7b228e5a975a9247820549012516a72a85f
Mateo Arboleda <marteomasoyyo@gmail.com>
2019-09-24 20:49:37 -0500

    Create Court model

--- backend/hoop/models.py
+++ backend/hoop/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Court(models.Model):
+    X3_3 = "3x3"
+    X4_4 = "4x4"
+    X4_5 = "4x5"
+
+    TYPE_CHOICES = (
+        (X3_3, '3x3'),
+        (X4_4, '4x4'),
+        (X4_5, '4x5')
+    )
+
+    name = models.CharField(max_length='100')
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    type = models.CharField(max_length=3, choices=TYPE_CHOICES)

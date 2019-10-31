816cdf2eed80d4eac2fc54f36bd400e331f02665
Carlosred <Carlos1002@hotmail.es>
2019-10-31 01:23:44 -0500

    nearest court done ,also fix loading main screen.

--- frontend/flutter/lib/main.dart
+++ frontend/flutter/lib/main.dart
@@ -11,7 +11,7 @@ import 'package:http/http.dart';
 import 'bt_cancha.dart';
 import 'mapas.dart';
 import 'nearestCourt.dart';
-
+import 'package:async_loader/async_loader.dart';
 import 'CourtCreator.dart';
 
 void main() => runApp(PrincipalPage());
@@ -23,33 +23,47 @@ Completer<GoogleMapController> _mapController = Completer();
 void _onMapCreated(GoogleMapController controller) {
   mapController = controller;
 }
-  var lng, lat;
-  bool showInfoCancha= false;
-  bool loading = true;
-  var tapped;
- 
+
+var lng, lat;
+bool showInfoCancha = false;
+bool loading = true;
+var tapped;
+
 Uint8List markerIcon;
 List<Mapa> mapas = List<Mapa>();
 
-class PrincipalPage extends StatefulWidget {
+class PrincipalPage extends StatelessWidget {
+  final GlobalKey<AsyncLoaderState> asyncLoaderState =
+      new GlobalKey<AsyncLoaderState>();
 
+  Future<List<Mapa>> getCourt() async {
+    print("si cogio el metodo");
+    Map<String, String> headers = {"Content-type": "application/json"};
+    String url = "https://hoop-back.herokuapp.com/api/v1/courts/";
+    // String json ='{"name":'+nameCourt+',"latitude":'+"$lat"+',"longitude":'+"$long"+',"type":'+type+'}';
 
- @override
-  State<StatefulWidget> createState() {
-    
+    Response response = await get(url, headers: headers);
+    //print(response.statusCode);
+    final json = jsonDecode(response.body);
+    //print(json[0]['latitude']);
 
-        return PrincipalPagestate();
-  }
-}
-class PrincipalPagestate extends State<PrincipalPage> {
-
-
-
+    json.forEach((j) {
+      Mapa m = new Mapa();
+      //print(j);
+      m.id = j['id'];
+      m.latitude = j['latitude'];
+      m.longitude = j['longitude'];
+      m.name = j['name'];
+      m.type = j['type'];
+      mapas.add(m);
+    });
+     
   
-   Future getLocation() async {
+  }
+
+  Future<List<Mapa>> getLocation() async {
     final location = Location();
     var currentLocation = await location.getLocation();
-    
 
     location.onLocationChanged().listen((LocationData currentLocation) {
       lat = currentLocation.latitude;
@@ -57,123 +71,91 @@ class PrincipalPagestate extends State<PrincipalPage> {
     });
     lat = currentLocation.latitude;
     lng = currentLocation.longitude;
-    
 
-    loading= false;
+    return getCourt();
   }
-  
-void initState(){
-  super.initState();
-  getLocation();
-loading= false;
 
-}
-
-getCourt() async {
-    print("si cogio el metodo");
-    Map<String, String> headers = {"Content-type": "application/json"};
-    String url= "https://hoop-back.herokuapp.com/api/v1/courts/";
-   // String json ='{"name":'+nameCourt+',"latitude":'+"$lat"+',"longitude":'+"$long"+',"type":'+type+'}';
+  Widget getNoConnectionWidget() {
     
-    Response response = await get(url,headers:headers);
-    //print(response.statusCode);
-    final  json = jsonDecode(response.body);
-    //print(json[0]['latitude']);
-    
-    json.forEach((j){
-      Mapa m = new Mapa();
-      //print(j);
-      m.id = j['id'];
-      m.latitude= j['latitude'];
-      m.longitude= j['longitude'];
-      m.name = j['name'];
-      m.type=j['type'];
-      mapas.add(m);
-
-    });
-
-    
-    //print(mapas[0].id);
-  }
-  @override
-  Widget build(BuildContext context) {
-   
-     
-      getCourt();
-
-   
-
-    return  Container(
-      alignment: Alignment.center,
-      child:  FutureBuilder(
-        future: getLocation(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (loading == false) {
-            
-            return  MaterialApp(home:  MainPage());
-          } else {
-            print(snapshot.error);
-            var location = new Location();
-            location.requestService();
-          
-            return  CircularProgressIndicator();
-            
-          }
-        },
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          height: 60.0,
+          child: new Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage('assets/sad.png'),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ),
+        new Text("oh oh algo salio mal =("),
+        new FlatButton(
+            color: Colors.red,
+            child: new Text(
+              "Intentar de nuevo",
+              style: TextStyle(color: Colors.blue,),
+            ),
+            onPressed: () => asyncLoaderState.currentState.reloadState())
+      ],
     );
   }
 
- 
-  
-  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    var _asyncLoader = new AsyncLoader(
+      key: asyncLoaderState,
+      initState: () async => await getLocation(),
+      renderLoad: () => Center(child: new CircularProgressIndicator()),
+      renderError: ([error]) => getNoConnectionWidget(),
+      renderSuccess: ({data}) => MainPage(canchas: data),
+    );
 
-
-  
+    return new MaterialApp(
+      
+      
+      
+      home: _asyncLoader,
+    );
+  }
 }
-  
-
-
- 
-
-
-
 
 class MainPage extends StatelessWidget {
+  List<Mapa> canchas;
+
+  MainPage({Key key, @required this.canchas}) : super(key: key);
 
   void addCourts(BuildContext context, List<Mapa> canchas) {
-    
+    canchas.forEach((c) {
+      var uuid = new Uuid();
+      var id = uuid.v4();
+      Marker _marker = new Marker(
+          icon: BitmapDescriptor.fromAsset("assets/court.png"),
+          markerId: MarkerId(id),
+          position: LatLng(c.latitude, c.longitude),
+          infoWindow: InfoWindow(title: c.name, snippet: c.type),
+          onTap: () {
+            print("si cogio");
+            showInfoCancha = !showInfoCancha;
+            print(showInfoCancha);
+          },
+          consumeTapEvents: tapped);
 
-  canchas.forEach((c){
-    var uuid = new Uuid();
-    var id = uuid.v4();
-Marker _marker = new Marker(
-        icon: BitmapDescriptor.fromAsset("assets/court.png"),
-        markerId: MarkerId(id),
-        position: LatLng(c.latitude, c.longitude),
-        infoWindow: InfoWindow(
-            title: c.name,
-            snippet: c.type),
-        onTap: () {
-          print("si cogio");
-         showInfoCancha= !showInfoCancha;
-         print(showInfoCancha);
-        },
-        consumeTapEvents: tapped);
-
-    markers[_marker.markerId] = _marker;
-
-  });
-    
+      markers[_marker.markerId] = _marker;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    addCourts(context,mapas);
-    return  Scaffold(
+    addCourts(context, mapas);
+    return Scaffold(
       resizeToAvoidBottomPadding: false,
-      appBar:  AppBar(
-        title:  Text("Hoop!"),
+      appBar: AppBar(
+        title: Text("Hoop!"),
       ),
 
       //esto aqui es para ir a donde se agregan las canchas.
@@ -221,7 +203,7 @@ Marker _marker = new Marker(
         ),
       ),
       bottomSheet: btCancha(showInfoCancha),
-      body:  Stack(
+      body: Stack(
         children: <Widget>[
           Container(
             child: GoogleMap(
@@ -261,4 +243,11 @@ Marker _marker = new Marker(
       ),
     );
   }
+}
+
+buildAppBarTitle(String title) {
+  return new Padding(
+    padding: new EdgeInsets.all(10.0),
+    child: new Text(title),
+  );
 }
--- frontend/flutter/lib/nearestCourt.dart
+++ frontend/flutter/lib/nearestCourt.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:flutter/cupertino.dart';
@@ -9,22 +8,24 @@ import 'dart:async';
 import 'package:http/http.dart';
 import 'package:map/main.dart' as prefix0;
 import 'mapas.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:latlong/latlong.dart'as calculate;
+import 'package:latlong/latlong.dart' as calculate;
+import 'package:async_loader/async_loader.dart';
+import 'main.dart';
 
-List<double> distancias= List<double>();
-List<String> nombres= List<String>();
+List<double> distancias = List<double>();
+List<String> nombres = List<String>();
 List<Mapa> mapas = List<Mapa>();
 void main() => runApp(NearestCourt());
- int meter=0;
-class NearestCourt extends StatelessWidget {
+int meter = 0;
 
- 
-  NearestCourt({Key key}): super(key:key);
+class NearestCourt extends StatelessWidget {
+  final GlobalKey<AsyncLoaderState> asyncLoaderState =
+      new GlobalKey<AsyncLoaderState>();
+  NearestCourt({Key key}) : super(key: key);
   double rest;
 
   // Variables para el control de la ubicacion del mapa e iconos
-  LatLng centro =  new LatLng(6.2322794, -75.6172466);
+  LatLng centro = new LatLng(6.2322794, -75.6172466);
   var lng, lat;
   var loading = true;
   var tapped;
@@ -41,92 +42,127 @@ class NearestCourt extends StatelessWidget {
     lat = currentLocation.latitude;
     lng = currentLocation.longitude;
     loading = false;
-    //print("Si corrio");
-    //print("Loading : $loading");
-    //print(lat);
-    //print(lng);
-//final Uint8List markerIcon = await getBytesFromAsset('iconos/cancha_opt.png', 100);
   }
-// METODO QUE ENVIA LA INFORMACION DE LA CANCHA Y LA CREA
-  
-//METODO ENCARGADO DE CREAR LOS MARKERS CON EL ICONO DE CANCHA EN EL MAPA
-  getCourt() async {
-    //print("si cogio el metodo");
+
+  bool back = false;
+  void goback(BuildContext context) {
+    mapas.clear();
+    distancias.clear();
+    nombres.clear();
+
+    Navigator.of(context).pop();
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => PrincipalPage()));
+  }
+
+  calculateDistances(List<Mapa> canchas) {
+    final calculate.Distance distance = new calculate.Distance();
+
+    canchas.forEach((c) {
+      rest = distance(new calculate.LatLng(prefix0.lat, prefix0.lng),
+          new calculate.LatLng(c.latitude, c.longitude));
+      distancias.add(rest);
+      nombres.add(c.name);
+    });
+    print(prefix0.lng);
+  }
+
+  Future<List<Mapa>> getCourt() async {
     Map<String, String> headers = {"Content-type": "application/json"};
-    String url= "https://hoop-back.herokuapp.com/api/v1/courts/";
-   // String json ='{"name":'+nameCourt+',"latitude":'+"$lat"+',"longitude":'+"$long"+',"type":'+type+'}';
-    
-    Response response = await get(url,headers:headers);
-    //print(response.statusCode);
-    final  json = jsonDecode(response.body);
-    //print(json[0]['latitude']);
-    
-    json.forEach((j){
+    String url = "https://hoop-back.herokuapp.com/api/v1/courts/";
+
+    Response response = await get(url, headers: headers);
+
+    final json = jsonDecode(response.body);
+
+    json.forEach((j) {
       Mapa m = new Mapa();
       //print(j);
       m.id = j['id'];
-      m.latitude= j['latitude'];
-      m.longitude= j['longitude'];
+      m.latitude = j['latitude'];
+      m.longitude = j['longitude'];
       m.name = j['name'];
-      m.type=j['type'];
+      m.type = j['type'];
       mapas.add(m);
-
     });
+    calculateDistances(mapas);
   }
 
-  calculateDistances(List<Mapa> canchas){
-
-
-     final calculate.Distance distance = new calculate.Distance();
-     
-    
-    canchas.forEach((c){
-      rest = distance.as(calculate.LengthUnit.Kilometer,
-      new calculate.LatLng(prefix0.lat, prefix0.lng),
-      new calculate.LatLng(c.latitude, c.longitude));
-    distancias.add(rest);
-    nombres.add(c.name);
-
-
-
-
-
-
-
-
-    });
-   
+  Widget getCourtList(List<Mapa> mapas) {
+    return ListView.builder(
+      itemCount: distancias.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          leading: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: 44,
+              minHeight: 44,
+              maxWidth: 64,
+              maxHeight: 64,
+            ),
+            child: Image.asset("assets/court.png", fit: BoxFit.cover),
+          ),
+          title: Text(nombres[index]),
+          subtitle: Text("Se encuentra a una distancia de " +
+              distancias[index].toString() +
+              " Metros"),
+        );
+      },
+    );
   }
 
+  Widget getNoConnectionWidget() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(
+          height: 60.0,
+          child: new Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage('assets/court.png'),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ),
+        new Text("oh oh algo salio mal =("),
+        new FlatButton(
+            color: Colors.red,
+            child: new Text(
+              "Retry",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () => asyncLoaderState.currentState.reloadState())
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    getCourt();
-   getLocation();
-   calculateDistances(mapas);
-    return Container(
-      alignment: Alignment.center,
-      child: FutureBuilder(
-        future: getLocation(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (loading == false) {
-            return ListView.builder(
-              itemCount: distancias.length,
-              itemBuilder: (context,index){
-                title: Text(nombres[index]);
-              },
-            );
-          } else {
-            print(snapshot.error);
-            var location = new Location();
-            location.requestService();
-            loading = false;
-            return new CircularProgressIndicator();
-          }
-        },
+    var _asyncLoader = new AsyncLoader(
+      key: asyncLoaderState,
+      initState: () async => await getCourt(),
+      renderLoad: () => Center(child: new CircularProgressIndicator()),
+      renderError: ([error]) => getNoConnectionWidget(),
+      renderSuccess: ({data}) => getCourtList(data),
+    );
+    return WillPopScope(
+      onWillPop: (){
+        goback(context);
+      },
+      child: Scaffold(
+        appBar: AppBar(title: buildAppBarTitle('Hoop!')),
+        body: Center(child: _asyncLoader),
       ),
     );
   }
 }
 
-
+buildAppBarTitle(String title) {
+  return new Padding(
+    padding: new EdgeInsets.all(10.0),
+    child: new Text(title),
+  );
+}
